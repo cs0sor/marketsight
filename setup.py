38386cbf141d6d7ae70b529dbb6609db5a959ebd
simon oram <simon@electrosoup.co.uk>
2020-03-20 11:39:38 +0000

    py3k version

--- setup.py
+++ setup.py
@@ -8,52 +8,46 @@ except ImportError:
     from distutils.core import setup
 
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [
-    'suds==0.4',
-    'wsgiref==0.1.2'
-]
+requirements = ["suds-community==0.8.3"]
 
 test_requirements = [
     # TODO: put package test requirements here
 ]
 
 setup(
-    name='marketsightapi',
-    version='0.1.0',
+    name="marketsightapi",
+    version="0.1.0",
     description="simple python wrapper round marketsight API",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     author="Simon Oram",
-    author_email='simon@electrosoup.co.uk',
-    url='https://github.com/cs0sor/marketsightapi',
-    packages=[
-        'marketsightapi'
-    ],
-    package_dir={'marketsightapi':
-                 'marketsight'},
+    author_email="simon@electrosoup.co.uk",
+    url="https://github.com/cs0sor/marketsightapi",
+    packages=["marketsightapi"],
+    package_dir={"marketsightapi": "marketsight"},
     include_package_data=True,
     install_requires=requirements,
     license="ISCL",
     zip_safe=False,
-    keywords='marketsightapi',
+    keywords="marketsightapi",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
-        'Natural Language :: English',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Natural Language :: English",
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    test_suite="tests",
+    tests_require=test_requirements,
 )
